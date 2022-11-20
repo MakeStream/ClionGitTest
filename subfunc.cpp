bdd9a199cc2eb360eca2cf2686a883b47c0c15ac
sunhw <sunhongwei@oray.com>
2022-11-21 01:28:57 +0800

    Add a second source file and modify main.cpp
    Signed-off-by: sunhw <sunhongwei@oray.com>

---
+++ subfunc.cpp
@@ -0,0 +1,5 @@
+//
+// Created by shw on 2022/11/21.
+//
+
+#include "subfunc.h"
---
+++ subfunc.h
@@ -0,0 +1,14 @@
+//
+// Created by shw on 2022/11/21.
+//
+
+#ifndef CLIONGITTEST_SUBFUNC_H
+#define CLIONGITTEST_SUBFUNC_H
+
+
+class subfunc {
+
+};
+
+
+#endif //CLIONGITTEST_SUBFUNC_H
